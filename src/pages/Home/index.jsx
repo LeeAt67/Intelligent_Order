06bb32e1ee69ad4d178f4c4203bf5a8ff8831250
LeeAt67 <1036232425@qq.com>
2025-08-03 17:02:59 +0800

    顶部搜索栏和AI

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,25 @@
 import useTitle from "@/hooks/useTitle";
-import styles from "./index.module.css";
+import Banner from "@/components/Banner";
+import Category from "@/components/Category";
+import Nav from "@/components/Nav";
+import Waterfall from "@/components/Waterfall";
 
 const Home = () => {
+  // 网页标题
   useTitle("首页");
-  return <div className={styles.home}>Home</div>;
+
+  return (
+    <>
+      {/* 顶部搜索栏 */}
+      <Nav />
+      {/* 轮播图 */}
+      <Banner />
+      {/* 分类导航 */}
+      <Category />
+      {/*瀑布流*/}
+      <Waterfall />
+    </>
+  );
 };
 
 export default Home;
